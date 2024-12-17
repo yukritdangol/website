ea8f19216e67ffa48e337638bd659f9501c67273
yukritdangol <yukritdangol@gmail.com>
2024-12-17 20:59:27 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+  function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex'
+  }
+
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none'
+  }
+
+  function message(){
+    alert("Pleased to help!");
+  }
+
+  function message1(){
+    alert("Thankyou for Contacting us");
+  }
